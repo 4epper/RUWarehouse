c3daa506a8155f73c133f4a82066daaae77f4371
4epper <121833387+4epper@users.noreply.github.com>
2023-11-01 01:56:37 -0400

    Add files via upload

---
+++ RUWarehouse/src/warehouse/Restock.java
@@ -0,0 +1,30 @@
+package warehouse;
+
+public class Restock {
+    public static void main(String[] args) {
+        StdIn.setFile(args[0]);
+        StdOut.setFile(args[1]);
+
+        Warehouse w = new Warehouse();
+
+        int size = StdIn.readInt();
+        for (int i=0; i < size; i++) {
+            String query = StdIn.readString();
+            if (query.equals("add")) {
+                int day = StdIn.readInt();
+                int id = StdIn.readInt();
+                String name = StdIn.readString();
+                int stock = StdIn.readInt();
+                int demand = StdIn.readInt();
+                w.addProduct(id, name, stock, day, demand);
+            } else {
+                int id = StdIn.readInt();
+                int amount = StdIn.readInt();
+                w.restockProduct(id, amount);
+            }
+        }
+        StdOut.println(w);
+
+	// Uset his file to test restock
+    }
+}
